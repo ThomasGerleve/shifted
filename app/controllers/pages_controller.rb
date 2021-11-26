602b7596c839f15f1f1bf7a66745a38f61b736c1
Thomas Gerleve <t.gerleve@web.de>
2021-11-26 16:28:17 +0100

    fixed login page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,7 @@
 class PagesController < ApplicationController
   def home
+    return unless user_signed_in?
+
     @next_shift = current_user.shifts.where('date > ?', DateTime.now).order(:date).first
     @next_shift_slot = @next_shift.slot.name
     @open_shifts = UserShift.all.select(&:open).map(&:shift).sort_by(&:date)
