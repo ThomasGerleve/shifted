c956e6dd2cc7b1ae4115a61614e07d67e2075b8f
Morgan Flament Wallin <morganflamentwallin@gmail.com>
2021-11-26 13:42:27 +0100

    Changed some naming, created loop for new-open shift

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,8 +5,8 @@ class PagesController < ApplicationController
   def dashboard
     @next_shift = current_user.shifts.where('date > ?', DateTime.now).order(:date).first
     @next_shift_slot = @next_shift.slot.name
-    @next_shift_start = @next_shift.slot.start_time
-    @next_shift_end = @next_shift.slot.end_time
+
+
 
 
   end
