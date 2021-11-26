fedaa70e20ce4d3bf9ee6ea02a3f1e61769d2d01
Karl von der Eltz <71014523+Keltz-dev@users.noreply.github.com>
2021-11-26 13:53:29 +0100

    Update user_shift.rb

--- app/models/user_shift.rb
+++ app/models/user_shift.rb
@@ -3,10 +3,4 @@ class UserShift < ApplicationRecord
   belongs_to :shift
 
   validates :details, presence: true
-
-  include PgSearch::Model
-  pg_search_scope :user_shift, associated_against: {
-    user: :user,
-    shift: :shift
-  }
 end
