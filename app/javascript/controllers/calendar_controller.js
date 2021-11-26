10a9143f61db64f4bd70f6decf36727e53635735
fullyouu <90682043+fullyouu@users.noreply.github.com>
2021-11-26 14:36:20 +0100

    navbar fixed

--- app/javascript/controllers/calendar_controller.js
+++ app/javascript/controllers/calendar_controller.js
@@ -1,6 +1,6 @@
 import { Controller } from "stimulus"
 
-export default class extends CalendarController {
+export default class extends Controller {
   connect() {
     console.log("Hello from your first Stimulus controller")
   }
