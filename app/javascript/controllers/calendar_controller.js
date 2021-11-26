5c6fc539895e076ef2acacc168fedc17ab83df4e
Karl von der Eltz <karlvondereltz@gmail.com>
2021-11-26 15:33:52 +0100

    finally done

--- app/javascript/controllers/calendar_controller.js
+++ app/javascript/controllers/calendar_controller.js
@@ -2,6 +2,9 @@ import { Controller } from "stimulus"
 
 export default class extends CalendarController {
   connect() {
-    console.log("Hello from your first Stimulus controller")
+    console.log("Hello")
+  }
+  disable(event) {
+    console.log(event);
   }
 }
