55fe78c17160e0d42f8c56d87128875c6dad8ff6
Karl von der Eltz <karlvondereltz@gmail.com>
2021-11-29 03:01:07 +0100

    AJAXiy and week align calendar

--- app/javascript/controllers/calendar_controller.js
+++ app/javascript/controllers/calendar_controller.js
@@ -1,7 +1,20 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-  connect() {
-    console.log("Hello")
-  }
+  static targets = ["container"]
+
+  navigate(event) {
+    event.preventDefault();
+
+    fetch(event.currentTarget.href, {
+      method: 'GET',
+      headers: { 'Accept': "application/json" },
+    })
+      .then(response => response.json())
+      .then((data) => {
+        console.log(data)
+        console.log(this.targets)
+        this.containerTarget.outerHTML = data.calendar;
+      });
+  };
 }
