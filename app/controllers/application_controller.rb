4c85bd3ea2d7ad942f3e6daeef14043691893fa2
fullyouu <90682043+fullyouu@users.noreply.github.com>
2021-11-30 14:44:47 +0100

    fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,15 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!, except: :home
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :set_up_background_image, if: :homepage?
+  before_action :light_background_image, if: :visitor_homepage?
 
-  def homepage?
-    return unless user_signed_in?
-      helpers.current_page?(controller: :pages, action: :home)
-    end
+  def visitor_homepage?
+    return !user_signed_in? && !devise_controller? &&
+           helpers.current_page?(controller: :pages, action: :home)
   end
 
-  def set_up_background_image
-    @page_container_class_name = "page-container-opacity"
+  def light_background_image
+    @page_container_class_name = "solid-background"
   end
 
   def configure_permitted_parameters
