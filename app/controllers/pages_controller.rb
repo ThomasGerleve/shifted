7444d6ad745e452b212879a31a14c724ea3d10e8
fullyouu <90682043+fullyouu@users.noreply.github.com>
2021-12-01 17:20:31 +0100

    notifications finished for own shift

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,8 +2,8 @@ class PagesController < ApplicationController
   def home
     return unless user_signed_in?
 
-    @shift_last_seen = session[:shift_last_seen]
-    session[:shift_last_seen] = DateTime.now
+    @last_seen_shift = session[:last_seen_shift]
+    session[:last_seen_shift] = DateTime.now
 
     @next_user_shift = current_user.user_shifts.select { |u_shift| u_shift.shift.date > DateTime.now }.sort_by { |u_shift| u_shift.shift.date }.first
     @emergency_shifts = UserShift.all.select { |u_shift| u_shift.open && u_shift.user_id == nil }.sort_by { |u_shift| u_shift.shift.date }
--- app/models/shift.rb
+++ app/models/shift.rb
@@ -2,10 +2,20 @@ class Shift < ApplicationRecord
   belongs_to :slot
   has_many :user_shifts
   has_many :users, through: :user_shifts
+  has_many :messages, through: :user_shifts
 
   validates :date, presence: true
 
   def user_on_shift?(user)
     users.include?(user)
+  end
+
+  def latest_message
+    messages.order(created_at: :desc).first
+  end
+
+  def unread_messages?(date, user)
+    message = latest_message
+    message && message.created_at > date && message.user != user
   end
 end
