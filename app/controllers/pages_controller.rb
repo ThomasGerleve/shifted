be48d0fd75414dd63e00ed6f536ef275a56d1485
fullyouu <90682043+fullyouu@users.noreply.github.com>
2021-12-01 16:51:20 +0100

    style - making code DRY & starting notifications

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,9 @@ class PagesController < ApplicationController
   def home
     return unless user_signed_in?
 
+    @shift_last_seen = session[:shift_last_seen]
+    session[:shift_last_seen] = DateTime.now
+
     @next_user_shift = current_user.user_shifts.select { |u_shift| u_shift.shift.date > DateTime.now }.sort_by { |u_shift| u_shift.shift.date }.first
     @emergency_shifts = UserShift.all.select { |u_shift| u_shift.open && u_shift.user_id == nil }.sort_by { |u_shift| u_shift.shift.date }
     @open_shifts = UserShift.all.select { |u_shift| u_shift.open && !u_shift.user_id.nil? }.sort_by { |u_shift| u_shift.shift.date }
