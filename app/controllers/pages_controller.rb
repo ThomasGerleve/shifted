d01be0afb4abec30b3a07fe143fa1f3785e2911e
Thomas Gerleve <t.gerleve@web.de>
2021-12-19 16:06:05 +0100

    added condition to not show open shifts from a past date to dashboard and calendar

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,8 +2,8 @@ class PagesController < ApplicationController
   def home
     return unless user_signed_in?
 
-    @next_user_shift = current_user.user_shifts.select { |u_shift| u_shift.shift.date > DateTime.now }.sort_by { |u_shift| u_shift.shift.date }.first
-    @emergency_shifts = UserShift.all.select { |u_shift| u_shift.open && u_shift.user_id == nil }.sort_by { |u_shift| u_shift.shift.date }
-    @open_shifts = UserShift.all.select { |u_shift| u_shift.open && !u_shift.user_id.nil? }.sort_by { |u_shift| u_shift.shift.date }
+    @next_user_shift = current_user.user_shifts.select { |u_shift| u_shift.shift.date > DateTime.now }.min_by { |u_shift| u_shift.shift.date }
+    @emergency_shifts = UserShift.all.select { |u_shift| u_shift.open && u_shift.user_id.nil? && u_shift.shift.date > DateTime.now }.sort_by { |u_shift| u_shift.shift.date }
+    @open_shifts = UserShift.all.select { |u_shift| u_shift.open && !u_shift.user_id.nil? && u_shift.shift.date > DateTime.now }.sort_by { |u_shift| u_shift.shift.date }
   end
 end
