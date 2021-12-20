9a05e2fb386453cd63f48e207693b8bc59a8bfe8
Thomas Gerleve <t.gerleve@web.de>
2021-12-20 19:37:31 +0100

    removed obsolete code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,16 +2,6 @@ class ApplicationController < ActionController::Base
   before_action :set_locale
   before_action :authenticate_user!, except: :home
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :light_background_image, if: :visitor_homepage?
-
-  def visitor_homepage?
-    return !user_signed_in? && !devise_controller? &&
-           helpers.current_page?(controller: :pages, action: :home)
-  end
-
-  def light_background_image
-    @page_container_class_name = "solid-background"
-  end
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
