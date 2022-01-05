7a62d3fdc8b1a072f7b26a93103787c79391ac34
Thomas Gerleve <t.gerleve@web.de>
2022-01-05 10:26:28 +0100

    setting up

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
   factory :user do
-    
+    first_name { "Testuser" }
+    last_name { "Rspec" }
+    experience_level { "Beginner" }
+    functional_role
+
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'validates correctly' do
+    user = create(:user)
+
+    expect(user).to eq ''
+  end
 end
